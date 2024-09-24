b614770f55f53d071fd13f452e9d65ba657a4fec
Joni Mustaniemi <mustaniemijm@gmail.com>
2024-09-24 10:49:24 +0300

    Optimizes images for mobile

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
-import realTimeMonitoring from "./images/real_time_monitoring.png";
-import dataSecurity from "./images/data_security.png";
-import analytics from "./images/analytics.png";
+import realTimeMonitoring from "./images/real_time_monitoring_optimized.png";
+import dataSecurity from "./images/data_security_optimized.png";
+import analytics from "./images/analytics_optimized.png";
 
 export default {
   realTimeMonitoring,
